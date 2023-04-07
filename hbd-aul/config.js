e36dd0598eb2036ecd3a9088adaa852c17d82bf5
ridwanmf21 <ridwanmaulana53@gmail.com>
2023-04-07 18:18:01 +0700

    first commit

---
+++ hbd-aul/config.js
@@ -0,0 +1,33 @@
+var config = {
+    texts: [
+        "Halooo, Aulia Harnum Aprilia Astri",      
+        "Selamat ulang tahun yang ke-21",  
+        "Semoga apa2 yg diharapkan dan ditargetkan",
+        "terwujud dalam waktu dekat:)",
+        "Tentunya dikabulkan dalam waktu yg tepat pula",
+        "Pokonya doa terbaik untuk kamu",
+        "Dimudahkan rezekinya,",
+        "Dipanjangkan umurnya",
+        "Dipermudah segala urusannya",
+        "Semoga sukses dan sehat selalu yaa!",
+        "Aamiin Allahumma Aamiin",
+        "I love you!"
+    ],
+
+    imgs: {
+        "Halooo, Aulia Harnum Aprilia Astri": "./imgs/aul.jpg",
+        "Semoga sukses dan sehat selalu yaa!": "./imgs/love.jpg",
+        "I love you!": "./imgs/duo.jpg"
+
+    },
+    desc: {
+        turn_on: "Haloooo, klik disini",
+        play: "Aku mau suprise :D",
+        bannar_coming: "Selamat ulang tahun",
+        balloons_flying: "Ada balon buat kamu nih",
+        cake_fadein: "Ada kuenya juga nih",
+        light_candle: "Yukk nyalain lilin nya",
+        wish_message: "Yeaayyyy!",
+        story: "Oiyaa lupa doanya",
+    }
+};
